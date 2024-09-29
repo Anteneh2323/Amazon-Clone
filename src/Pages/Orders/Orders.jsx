bc01fdd1428f03819010954fa08e33c275508bef
Anteneh2323 <145783826+Anteneh2323@users.noreply.github.com>
2024-09-29 13:44:34 -0500

    fix a typo

--- src/Pages/Orders/Orders.jsx
+++ src/Pages/Orders/Orders.jsx
@@ -3,7 +3,7 @@ import Layout from "../../Components/Layout/Layout";
 import classes from "./Orders.module.css";
 import { db } from "../../Utility/firebase";
 import { DataContext } from "../../Components/DataProvider/DataProvider";
-import ProductCard from "../../components/Product/ProductCard";
+import ProductCard from "../../Components/Product/ProductCard";
 import {
   collection,
   doc,
